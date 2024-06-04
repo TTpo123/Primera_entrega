cd57d62c2d6765762ee8309175e6c6968f93006c
TTpo123 <129268785+TTpo123@users.noreply.github.com>
2024-06-03 21:16:00 -0600

    Version 0.4

--- app/src/main/java/com/Equipo3/actividad/MainActivity.kt
+++ app/src/main/java/com/Equipo3/actividad/MainActivity.kt
@@ -1,19 +1,74 @@
 package com.Equipo3.actividad
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var signInButton: Button
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        emailEditText = findViewById(R.id.emailEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+        signInButton = findViewById(R.id.signInButton)
+
+        signInButton.setOnClickListener { signIn() }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
 }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser: FirebaseUser? = mAuth.currentUser
+        if (currentUser != null) {
+            Log.d("MainActivity", "User is signed in: ${currentUser.email}")
+        }
+    }
+    private fun signIn() {
+        val email = emailEditText.text.toString()
+        val password = passwordEditText.text.toString()
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Ingrese su usuario y contraseña.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d("MainActivity", "signInWithEmail:success")
+                    val user: FirebaseUser? = mAuth.currentUser
+                    Toast.makeText(this, "Sesion iniciada con exito.", Toast.LENGTH_SHORT).show()
+                } else {
+                    Log.w("MainActivity", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(this, "Usuario o contraseña incorrectos.", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+    }
